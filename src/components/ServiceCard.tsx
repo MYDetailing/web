3721fd083caf6a6876afd0c9252f76887edfaf25
Eddie <eddiebu.1515@gmail.com>
2025-06-21 20:17:37 -0500

    create InfoToolTipText component
    and use it in ServiceCard

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,10 +1,11 @@
 // design of one service card, used in service section
 
-import { Box, ClickAwayListener, Stack, Tooltip, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import { useState } from "react";
+
 import { SERVICE_CARD_STYLE, SERVICE_CARD_CONTENT_STYLE } from "../constants/styles";
+import InfoToolTipText from "./InfoToolTipText";
 
 interface Props {
   heading: string;
@@ -15,10 +16,6 @@ interface Props {
   hints: string[];
   previousPackage: string;
 }
-
-const infoImageStyle = {
-  height: "1rem",
-};
 
 function ServiceCard({
   heading,
@@ -45,49 +42,9 @@ function ServiceCard({
             <Typography variant="body1">Everything included in {previousPackage}, plus</Typography>
           )}
           <Stack>
-            {services.map((service, index) => {
-              const [open, setOpen] = useState(false);
-
-              function handleOpenTooltip() {
-                setOpen(true);
-              }
-              0;
-
-              function handleCloseTooltip() {
-                setOpen(false);
-              }
-
-              return (
-                <ClickAwayListener onClickAway={handleCloseTooltip} key={index}>
-                  <Tooltip
-                    open={open}
-                    onClose={handleCloseTooltip}
-                    disableFocusListener
-                    disableHoverListener
-                    disableTouchListener
-                    title={service + ": " + hints[index]}
-                    arrow
-                    placement="bottom"
-                    enterDelay={500}
-                    slotProps={{
-                      tooltip: { sx: { fontSize: "1.2rem" } },
-                      popper: {
-                        modifiers: [{ name: "offset", options: { offset: [0, -14] } }],
-                      },
-                    }}
-                  >
-                    <Stack flexDirection={"row"} alignItems={"center"}>
-                      <Typography variant="body1" key={index}>
-                        {service}
-                      </Typography>
-                      <Box flexGrow={1} />
-
-                      <img src="/info.png" style={infoImageStyle} onClick={handleOpenTooltip} />
-                    </Stack>
-                  </Tooltip>
-                </ClickAwayListener>
-              );
-            })}
+            {services.map((service, index) => (
+              <InfoToolTipText tip={service + ": " + hints[index]} text={service} />
+            ))}
           </Stack>
         </Stack>
       </CardContent>
