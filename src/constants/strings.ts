e01f3df64e361539501df9dfe79530603270db69
Eddie <eddiebu.1515@gmail.com>
2025-06-21 20:20:52 -0500

    create page that shows all adds-ons

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,6 +1,7 @@
 // strings that are displayed somewhere on the site or used in the code
 
 export const NAV_BAR_SECTIONS = ["Packages", "Contact"];
+export const SERVICE_CATEGORIES = ["Interior", "Exterior", "Restoration"]
 
 export const VISION = "AT MY DETAILING, WE BELIEVE YOUR VEHICLE DESERVES MORE THAN JUST A WASH. " + 
     "OUR EXPERT SERVICES REJUVENATE, PROTECT, AND ELEVATE YOUR " +
@@ -10,7 +11,8 @@ export const SLOGAN = "We Clean You Shine";
 export const TITLE = "MY Detailing";
 export const SUBTITLE = "Premium Car Detailing in Winkler!"
 
-export const VEHICLE_TYPES = ["SEDAN", "SUV OR PICKUP", "MINIVAN", "SEMI TRUCK", "RV OR CAMPER"];
+export const ALL_VEHICLE_TYPES = ["SEDAN", "SUV OR PICKUP", "MINIVAN", "SEMI TRUCK", "RV OR CAMPER"];
+export const CAR_TYPES = ["SEDAN", "SUV OR PICKUP", "MINIVAN"];
 export const CUSTOM_PKG_TITLE = "CUSTOM";
 export const CUSTOM_PKG_DESCRIPTION = "Looking for a custom package? Start with any of our existing packages and customize it by adding or removing services—or explore all services and add-ons to build your own. Then just reach out to finalize your personalized detailing package.";
 export const ALL_SERVICES_BTN_TXT = "view services and add-ons";
--- src/pages/ServicesPage.tsx
+++ src/pages/ServicesPage.tsx
@@ -1,47 +1,167 @@
 // page that shows all services and add-ons
-import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+
+import { CSSProperties, useState } from "react";
+import {
+  Box,
+  Collapse,
+  IconButton,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+
+import { SERVICE_CATEGORIES } from "../constants/strings.ts";
+import { CAR_TYPES } from "../constants/strings.ts";
+import { H6_STYLE } from "../constants/styles.ts";
+import { DEFAULT_VEHICLE_TYPE } from "../constants/values.ts";
+
+import SelectorTabs from "../components/SelectorTabs.tsx";
+import InfoToolTip from "../components/InfoToolTipText.tsx";
+
 import serviceData from "../data/services.json";
 import { Service } from "../types.ts";
-import { H6_STYLE } from "../constants/styles.ts";
+import { PURE_WHITE } from "../constants/colors.ts";
 
 const allServices: Service[] = serviceData.services;
 
+const wrapperBoxStyle: CSSProperties = {
+  padding: "20px 10px",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+};
+
+const iconCellStyle: CSSProperties = {
+  width: "32px",
+};
+
+const iconStyle: CSSProperties = {
+  color: PURE_WHITE,
+};
+
+const collapsableCellStyle: CSSProperties = { borderBottom: 0 };
+
+const descriptionCellStyleDesktop = {
+  display: {
+    xs: "none",
+    sm: "table-cell",
+  },
+};
+
+const descriptionCellStyleMobile = {
+  display: {
+    xs: "table-cell",
+    sm: "none",
+  },
+};
+
 export default function ServicesPage() {
-    const servicesToShow: Service[] = allServices.filter(
-        (service) => service.category != undefined
-    );
-    return (
-        <Box padding="20px 10px" display="flex" flexDirection="column" alignItems="center">
-            <Typography variant="h4">
-                Add-Ons
-            </Typography>
-            <Typography variant="h6" sx={H6_STYLE}>
-                Add these options to any package—or combine them to build your own.
-            </Typography>
+  // create a 2D array of services, where the services are split into interior, exterior and restoration
+  const servicesToShow: Service[][] = [];
+  SERVICE_CATEGORIES.forEach((serviceCategory) => {
+    servicesToShow.push(allServices.filter((service) => service.category == serviceCategory));
+  });
 
-        <Table>
-            <TableHead>
-                <TableRow>
-                    <TableCell>Service</TableCell>
-                    <TableCell>Description</TableCell>
-                    <TableCell align="right">Prices (Sedan/SUV/Pickup)</TableCell>
-                </TableRow>
-            </TableHead>
+  const [visibleCategories, setVisibleCategories] = useState<boolean[]>(Array(3).fill(false));
 
-            <TableBody>
-                {
-                    servicesToShow.map((service) => (
-                        <TableRow>
-                            <TableCell>{service.name}</TableCell>
-                            <TableCell>{service.description}</TableCell>
-                            <TableCell align="right">{service.prices?.join("/")}</TableCell>
-                        </TableRow>
-                    ))
-                }
-            </TableBody>
+  const [vehicleType, setVehicleType] = useState(DEFAULT_VEHICLE_TYPE);
 
-        </Table>
-        </Box>
+  function handleVehicleTypeChange(event: React.SyntheticEvent, newValue: number) {
+    setVehicleType(newValue);
+  }
 
-    );
+  function toggleSectionExpansion(index: number) {
+    setVisibleCategories((prev) => prev.map((val, i) => (i === index ? !val : val)));
+  }
+
+  return (
+    <Box sx={wrapperBoxStyle}>
+      <Typography variant="h4">Add-Ons</Typography>
+      <Typography variant="h6" sx={H6_STYLE}>
+        Add these options to any package—or combine them to build your own.
+      </Typography>
+
+      <Box height={"2rem"} />
+
+      <SelectorTabs
+        title={"Vehicle Type"}
+        selectedOption={vehicleType}
+        onChange={handleVehicleTypeChange}
+        allOptions={CAR_TYPES}
+      />
+
+      <Table>
+        <TableBody>
+          {servicesToShow.map((services: Service[], index) => (
+            <>
+              {/* Category row */}
+              <TableRow>
+                <TableCell sx={iconCellStyle}>
+                  <IconButton
+                    aria-label="expand row"
+                    size="small"
+                    onClick={() => toggleSectionExpansion(index)}
+                  >
+                    {visibleCategories[index] ? (
+                      <KeyboardArrowUpIcon sx={iconStyle} />
+                    ) : (
+                      <KeyboardArrowDownIcon sx={iconStyle} />
+                    )}
+                  </IconButton>
+                </TableCell>
+                <TableCell onClick={() => toggleSectionExpansion(index)}>
+                  <Typography>{services[0].category}</Typography>
+                </TableCell>
+              </TableRow>
+              {/* Category row end */}
+
+              {/* Collapsable row */}
+              <TableRow>
+                <TableCell sx={collapsableCellStyle} colSpan={6}>
+                  <Collapse key={services[0].category} in={visibleCategories[index]}>
+                    <Box>
+                      <Table>
+                        <TableHead>
+                          <TableRow>
+                            <TableCell>Service</TableCell>
+                            <TableCell sx={descriptionCellStyleDesktop}>Description</TableCell>
+                            <TableCell sx={descriptionCellStyleMobile}></TableCell>
+                            <TableCell align="right">Prices</TableCell>
+                          </TableRow>
+                        </TableHead>
+
+                        <TableBody>
+                          {services.map((service) => (
+                            <TableRow key={service.id}>
+                              <TableCell>{service.name}</TableCell>
+                              <TableCell sx={descriptionCellStyleDesktop}>
+                                {service.description}
+                              </TableCell>
+                              <TableCell sx={descriptionCellStyleMobile}>
+                                <InfoToolTip tip={service.name + ": " + service.description} />
+                              </TableCell>
+                              <TableCell align="right">
+                                {service.prices?.[vehicleType] ?? ""}
+                              </TableCell>
+                            </TableRow>
+                          ))}
+                        </TableBody>
+                      </Table>
+                    </Box>
+                  </Collapse>
+                </TableCell>
+              </TableRow>
+              {/* Collapsable row end */}
+            </>
+          ))}
+        </TableBody>
+      </Table>
+    </Box>
+  );
 }
