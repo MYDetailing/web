11973f34f75b981fb549f3b55ad01be3e8de5685
Eddie <eddiebu.1515@gmail.com>
2025-10-20 18:21:29 -0700

    create WindowTintPage and ExteriorGlossPage

---
+++ src/pages/ExteriorGlossPage.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import NavBar from "../components/NavBar";
+
+export default function ExteriorGlossPage() {
+  return (
+    <Box>
+      <NavBar />
+    </Box>
+  );
+}
---
+++ src/pages/WindowTintPage.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import NavBar from "../components/NavBar";
+
+export default function WindowTintPage() {
+  return (
+    <Box>
+      <NavBar />
+    </Box>
+  );
+}
