a1aadcf8c7f8f7acfa154c91de58d27971ac39cd
Eddie <eddiebu.1515@gmail.com>
2025-10-28 14:39:36 -0500

    refactor resourceLocations for consistency

--- src/constants/resourceLocations.ts
+++ src/constants/resourceLocations.ts
@@ -12,8 +12,8 @@ export const GOOGLE_MAPS_LINK = "https://maps.app.goo.gl/wULyaj4xL5Cpwtpt5";
 export const INSTA_LINK = "https://www.instagram.com/mydetailingca/#";
 export const WEBSITE_LINK = "https://mydetailing.ca";
 export const CONTACT_SECTION = "/#Contact";
-export const TINT_CAR_PNG = "/TintCar.png";
-export const TINT_CAR_SM_PNG = "/TintCarSm.png";
+export const TINT_CAR_PNG = "/tintcar.png";
+export const TINT_CAR_SM_PNG = "/tintcarsm.png";
 
 // routes
 export const ROUTES = {
